5f0329ab1a831d66a861c97739a0b324fc50ec81
David Gamez <david.gamez@gmail.com>
2015-11-12 22:22:15 -0500

    Added MQTT skeleton class

---
+++ plugins/MQTT.js
@@ -0,0 +1,67 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+function MQTT() {
+}
+
+/**
+ * Method called from the bridge at starting time. It's used to initialize the plugin and receive the initial configuration.
+ * 
+ * @param bc bridge instance
+ * @param thingTypeCnfg info
+ * @param thingInfo details
+ * @param complete function 
+ */
+MQTT.prototype.start = function (bc, thingTypeCnfg, thingInfo, complete) {
+    try {
+        var self = this;
+    } catch (e) {
+        complete(e);
+    }
+};
+
+/**
+ * Method called from the bridge to stop the work session. It's used to release resources like network connections.
+ * 
+ * @param complete function
+ */
+MQTT.prototype.stop = function (complete) {
+    try {
+
+        complete(null);
+    } catch (e) {
+        complete(e);
+    }
+};
+
+/**
+ * Method called from the bridge to propagate an observation or a list of observations.
+ * 
+ * @param observation update
+ * @param complete function
+ */
+MQTT.prototype.update = function (observation, complete) {
+    try {
+        var self = this;
+    } catch (e) {
+        complete(e);
+    }
+};
+
+/**
+ * 
+ * 
+ * @param observation
+ * @param complete
+ */
+MQTT.prototype.command = function (observation, complete) {
+    try {
+
+    } catch (e) {
+        complete(e);
+    }
+};
+
+
+module.exports.MQTT = MQTT;
